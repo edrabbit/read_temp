9ed269f5bb1db4e08bba9cf71f45a0226f2bfec1
Ed Hunsinger <edrabbit@edrabbit.com>
2013-04-09 22:39:08 -0700

    initial commit

---
+++ read_temp.py
@@ -0,0 +1,48 @@
+"""read_temp.py
+
+Read the temperature from a DS18B20 temperature sensor on a Raspberry Pi
+
+This will print a Splunk-friendly key/value pair log line.
+"""
+
+__author__ = "Ed Hunsinger"
+__email__ = "edrabbit@edrabbit.com"
+
+import datetime
+import glob
+import os
+import time
+
+import pytz
+
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
+
+base_dir = '/sys/bus/w1/devices/'
+device_folder = glob.glob(base_dir + '28*')[0]
+device_file = device_folder + '/w1_slave'
+
+def read_temp_raw():
+    f = open(device_file, 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
+
+def read_temp():
+    lines = read_temp_raw()
+    utc_time = datetime.datetime.utcnow().replace(tzinfo=pytz.utc).isoformat()
+
+    while lines[0].strip()[-3:] != 'YES':
+        time.sleep(0.2)
+        lines = read_temp_raw()
+    (serial_num, temp_string) = lines[1].rstrip().split(' t=')
+    temp_c = float(temp_string) / 1000.0
+    temp_f = temp_c * 9.0 / 5.0 + 32.0
+    return utc_time, serial_num, temp_c, temp_f
+
+
+while True:
+    utc_time, serial_num, temp_c, temp_f = read_temp()
+    print ('%s serial_number="%s", temp_c=%.2f, temp_f=%.2f'
+          % (utc_time, serial_num, temp_c, temp_f))
+    time.sleep(1)
